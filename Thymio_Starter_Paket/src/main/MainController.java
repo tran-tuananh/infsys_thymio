eea3685cf69dc05e8c639ec90bce6297d08f8210
tran-tuananh <tuananh.tran@gmx.de>
2015-07-10 22:05:09 +0200

    Code für wide-Format
    Zusätzlichen Code um Sensordaten im wide-Format zu speichern

--- Thymio_Starter_Paket/src/main/MainController.java
+++ Thymio_Starter_Paket/src/main/MainController.java
@@ -1,4 +1,4 @@
- package main;
+package main;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -12,10 +12,11 @@ public class MainController {
 	/**
 	 * TODO: Hier die IP-Adresse des Raspberries eingeben!
 	 */
-	private static final String IP = "192.168.43.170"; // Tutor-AJ:											// 192.168.43.100
+	private static final String IP = "192.168.43.170"; // Tutor-AJ: //
+														// 192.168.43.100
 	private static final String DELIMITER = ",";
 	private static final String NEW_LINE = "\n";// Tutor-TK:
-														// 172.20.10.13
+												// 172.20.10.13
 	public Thymio myThymio;
 
 	public MainController(String host) {
@@ -23,18 +24,28 @@ public class MainController {
 	}
 
 	public void run() {
-		//Datei umbenennen
+		// Datei umbenennen
 		File csv = new File("ecke.csv");
 		FileWriter fw;
 		try {
 			fw = new FileWriter(csv);
-			fw.write("Sensor,Value"+NEW_LINE);
-			
-			for (int i = 0; i < 750; i++){
-				ArrayList <Short>sensorArray = (ArrayList<Short>)myThymio.getProxHorizontal();
+			// fw.write("Sensor,Value"+NEW_LINE); //Head for long format
+			fw.write("s0,s1,s2,s3,s4" + NEW_LINE); // Head for wide format
+
+			for (int i = 0; i < 750; i++) {
+				ArrayList<Short> sensorArray = (ArrayList<Short>) myThymio
+						.getProxHorizontal();
 				System.out.println(sensorArray);
-				for(int j = 0; j < sensorArray.size()-2; j++){
-					fw.append("s"+j+DELIMITER+sensorArray.get(j)+NEW_LINE);
+				for (int j = 0; j < sensorArray.size() - 2; j++) {
+					//Values in long format
+					// fw.append("s"+j+DELIMITER+sensorArray.get(j)+NEW_LINE);
+					
+					//Values in wide format
+					if (j == sensorArray.size() - 3) {
+						fw.append(sensorArray.get(j) + NEW_LINE);
+					} else {
+						fw.append(sensorArray.get(j) + DELIMITER);
+					}
 				}
 			}
 			fw.flush();
@@ -42,9 +53,7 @@ public class MainController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
+
 	}
 
 	public static void main(String[] args) {
